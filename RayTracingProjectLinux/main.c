51c29315008467e298dc541ba9cd7a24dcbfcfd0
cyprienmaes <cyprienmaes@gmail.com>
2018-04-18 16:53:42 +0200

    nouvelle fonction pour retrouver l'image d'un point selon le sens du mur

--- RayTracingProjectLinux/main.c
+++ RayTracingProjectLinux/main.c
@@ -8,6 +8,7 @@
 #include "line.h"
 #include "reflection.h"
 #include "TXandRX.h"
+#include "point.h"
 
 int main(int argc, char *argv[]) {
     WALL* wall = NULL;
@@ -43,6 +44,7 @@ int main(int argc, char *argv[]) {
     SDL_WM_SetCaption("Projet de Ray-Tracing v0.0.3", NULL);
 
     wall = SquareMap(screen, wall);
+
     transmitter = newTransmitter(20,500,transmitter,screen);
     receiver = newReceiver(500, 250, receiver, screen);
     // doubleReflection(receiver, transmitter, wall, screen);
@@ -58,32 +60,4 @@ int main(int argc, char *argv[]) {
     // Stoping of SDL
     SDL_Quit();
     return EXIT_SUCCESS;
-}
-
-void doubleReflection(RECEIVER *receiver, TRANSMITTER *transmitter, WALL *wall, SDL_Surface *screen) {
-    int dy, dx, i, xIm1, yIm1, xIm2, yIm2, yMur2, xMur2, yMur1, xMur1;
-    int yWall1 = wall[1].position.y;
-    int xWall1 = wall[1].position.x;
-    int yWall2 = wall[3].position.y;
-    int xWall2 = wall[3].position.x;
-    int xTX = transmitter->position.x+5;
-    int yTX = transmitter->position.y+5;
-    int xRX = receiver->position.x+5;
-    int yRX = receiver->position.y+5;
-    float tgPhi, tgTheta;
-    dy = yWall1 - yTX;
-    xIm1 = xTX;
-    yIm1 = yTX + 2*dy;
-    dx = xWall2 - xIm1;
-    xIm2 = xIm1 + 2*dx;
-    yIm2 = yIm1;
-    xMur2 = xWall2;
-    tgPhi = (float) (xIm2-xRX)/(yIm2-yRX);
-    yMur2 = (xMur2 - xRX + (yRX*tgPhi))/tgPhi;
-    yMur1 = yWall1;
-    tgTheta = (float) (yIm1-yMur2)/(xMur2-xIm1);
-    xMur1 = ((tgTheta*xMur2)-yMur1+yMur2)/tgTheta;
-    line(xTX, yTX, xMur1, yMur1, SDL_MapRGB(screen->format,74,152,89), screen);
-    line(xMur1, yMur1, xMur2, yMur2, SDL_MapRGB(screen->format,74,152,89), screen);
-    line(xMur2, yMur2, xRX, yRX, SDL_MapRGB(screen->format,74,152,89), screen);
 }
