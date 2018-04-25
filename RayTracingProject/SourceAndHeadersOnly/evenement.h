86c91dc47dfce687eab94a64f52cf6f08d18a056
JohnAntoun <antoun.john@hotmail.be>
2018-04-25 17:27:50 +0200

    Synchronisation avec new branch

---
+++ RayTracingProject/SourceAndHeadersOnly/evenement.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+
+#ifndef EVENEMENT_H_INCLUDED
+#define EVENEMENT_H_INCLUDED
+
+
+
+#endif // EVENEMENT_H_INCLUDED
+
+void posSouris(const char* file, int taillePolice,int posXx, int posYx, int posXy, int posYy, SDL_Surface *screen);
